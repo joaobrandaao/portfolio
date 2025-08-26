dd0badf9d1cb596bac8d743170b72e28d9b2c335
joaobrandaao <160801920+joaobrandaao@users.noreply.github.com>
2025-08-26 02:24:19 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('menu-btn');
-const nav = document.getElementById('nav').querySelector('ul');
-
-menuBtn.addEventListener('click', () => {
-  nav.classList.toggle('show');
-});
