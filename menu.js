5cb5f8ecd442244b7d129691d540f0a5d145c6cc
joaobrandaao <joaovitorbrandaoteixeira11@gmail.com>
2025-08-27 01:47:22 -0300

    arrumando menu para fechar pos clique

--- menu.js
+++ menu.js
@@ -1,6 +1,15 @@
 const menuBtn = document.getElementById('menu-btn');
 const nav = document.getElementById('nav').querySelector('ul');
+const navLinks = nav.querySelectorAll('a'); // Seleciona todos os links dentro do menu
 
+// Abre e fecha o menu ao clicar no botão
 menuBtn.addEventListener('click', () => {
   nav.classList.toggle('show');
+});
+
+// Fecha o menu quando um link é clicado
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show');
+  });
 });
